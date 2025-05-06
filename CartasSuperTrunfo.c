4a3c1a31559c8d1d9b39bfc0e71b73c5dfa24150
wds0809 <wds0809@gmail.com>
2025-05-06 13:57:35 +0000

    Conclusão Desafio nível Mestre - Wanderson

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -20,6 +20,17 @@ int main() {
     float areaC1, pibC1, areaC2, pibC2; // Valores decimais: área e PIB
     float densidadeC1, densidadeC2;
     float pibPerCapitaC1, pibPerCapitaC2;
+    float superPoderC1, superPoderC2;
+
+    // Comparar as Cartas
+    
+    int comparacaoPopulacao;
+    int comparacaoArea;
+    int comparacaoPib;
+    int comparacaoPontosTuristicos;
+    int comparacaoDensidade;
+    int comparacaoPibPerCapita;
+    int comparacaoSuperPoder;
      
     printf ("Digite uma letra de A a H para representação de um estado para Carta 1: \n");
     scanf (" %c", &estadoC1); // O espaço antes do %c evita problemas com \n no buffer
@@ -48,6 +59,9 @@ int main() {
     printf ("Digite quantos pontos turísticos existe na cidade: \n");
     scanf ("%d", &nPontosC1);
 
+    // Super poder
+    superPoderC1 = populacaoC1 + areaC1 + pibC1 + (1 / pibPerCapitaC1) + nPontosC1;
+
     printf ("Digite uma letra de A a H para representação de um estado para Carta 2: \n");
     scanf (" %c", &estadoC2); // O espaço antes do %c evita problemas com \n no buffer
 
@@ -75,18 +89,31 @@ int main() {
     printf ("Digite quantos pontos turísticos existe na cidade: \n");
     scanf ("%d", &nPontosC2);
 
+    // Super poder
+    superPoderC2 = populacaoC2 + areaC2 + pibC2 + (1 / pibPerCapitaC2) + nPontosC2;
+
+    // Comparação
+    comparacaoPopulacao = populacaoC1 > populacaoC2;
+    comparacaoArea = areaC1 > areaC2;
+    comparacaoPib = pibC1 > pibC2;
+    comparacaoPontosTuristicos = nPontosC1 > nPontosC2;
+    comparacaoDensidade = densidadeC1 < densidadeC2; // Densidade populacional, a carta com o menor valor vence
+    comparacaoPibPerCapita = pibPerCapitaC1 > pibPerCapitaC2;
+    comparacaoSuperPoder = superPoderC1 > superPoderC2;
+
     // Resultados das Cartas
 
     printf("\n--- Carta 1 ---\n");
     printf("Estado: %c\n", estadoC1);
     printf("Código: %c%s\n", estadoC1, codCartaC1); // Combina o estado (letra) com o código da carta para gerar algo como A01
     printf("Nome da Cidade: %s\n", nomeC1);
-    printf("População: %d\n", populacaoC1);
+    printf("População: %d habitantes \n", populacaoC1);
     printf("Área: %.2f km²\n", areaC1); // Usa %.2f para mostrar 2 casas decimais.
     printf("PIB: %.2f bilhões de reais\n", pibC1); // Usa %.2f para mostrar 2 casas decimais.
     printf("Número de Pontos Turísticos: %d\n", nPontosC1);
     printf("Densidade Populacional: %.2f hab/km²\n", densidadeC1);
-    printf("PIB per Capita: %.2f reais", pibPerCapitaC1);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapitaC1);
+    printf("Super Poder: %.2f pontos\n", superPoderC1);
 
     printf("\n--- Carta 2 ---\n");
     printf("Estado: %c\n", estadoC2);
@@ -97,7 +124,18 @@ int main() {
     printf("PIB: %.2f bilhões de reais\n", pibC2); // Usa %.2f para mostrar 2 casas decimais
     printf("Número de Pontos Turísticos: %d\n", nPontosC2);
     printf("Densidade Populacional: %.2f hab/km²\n", densidadeC2);
-    printf("PIB per Capita: %.2f reais", pibPerCapitaC2);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapitaC2);
+    printf("Super Poder: %.2f pontos\n", superPoderC2);
+
+    // Imprimir Comparação de Cartas
+    printf("\n--- Comparação de Cartas ---\n");
+    printf("População: %d\n", comparacaoPopulacao);
+    printf("Área: %d\n", comparacaoArea);
+    printf("PIB: %d\n", comparacaoPib);
+    printf("Pontos Turísticos: %d\n", comparacaoPontosTuristicos);
+    printf("Densidade Populacional: %d\n", comparacaoDensidade);
+    printf("PIB per Capita: %d\n", comparacaoPibPerCapita);
+    printf("Super Poder: %d\n", comparacaoSuperPoder);
 
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
